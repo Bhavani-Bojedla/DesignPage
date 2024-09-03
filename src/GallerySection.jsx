24e9ea1384c236f20d5749c00bcad228db2ad108
BHAVANIBOJEDLA <bhavanibojadla8@gmail.com>
2024-09-03 10:45:23 +0530

    your message

--- src/GallerySection.jsx
+++ src/GallerySection.jsx
@@ -5,9 +5,9 @@ export default function GallerySection() {
     return (
       <div className="flex flex-col mt-6 ">
               <div className="flex justify-between 2560:w-100 lg:w-98 ">
-                <img  className="w-40 h-40 bg-cover bg-rect rounded-3xl" />
-                <img  className="w-40 h-40 bg-cover bg-rect rounded-3xl" />
-                <img  className="w-40 h-40 bg-cover bg-rect rounded-3xl" />
+                <img  className="bg-cover h-36 w-36 bg-rect rounded-3xl" />
+                <img  className="bg-cover h-36 w-36 bg-rect rounded-3xl" />
+                <img  className="bg-cover h-36 w-36 bg-rect rounded-3xl" />
               </div>
       </div>
     );
